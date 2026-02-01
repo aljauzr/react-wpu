de06688f02003b364dacff2e2587dc91f15139d7
Al Jauzi Abdurrohman <aljauzir@gmail.com>
2026-02-01 16:39:55 +0700

    refactor: remove unused assets and styles, update App component structure

--- 2-instalasi-dan-konfigurasi/src/App.jsx
+++ 2-instalasi-dan-konfigurasi/src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+function Header({author}) {
+    return <h1 className="heading">Belajar React bareng {author ? author: "Pa Dhika"}</h1>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const students = ["Dhika", "Joji", "Erik"];
+  const [likes, setLikes] = useState(0);
+  
+  function handleClick() {
+      setLikes(likes + 1);
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Header author="WPU" />
+          <ul>
+              {students.map((student) => (
+                  <li key="student">{student}</li>
+              ))}
+          </ul>
+
+          <button onClick={handleClick}>Likes ({likes})</button>
       </div>
-      <h1>Welcome to WPU! 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
